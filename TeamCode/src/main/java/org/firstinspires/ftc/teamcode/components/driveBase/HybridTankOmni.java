acd1c4abcae4598d5531d579f9fc1ad3cac26d7a
Edward Yang <edward.yang6771@gmail.com>
2018-10-09 20:16:20 -0500

    Updated drivebase code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/driveBase/HybridTankOmni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/driveBase/HybridTankOmni.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.components.driveBase;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
@@ -13,9 +14,15 @@ class HybridTankOmni extends DriveBase {
     Telemetry telemetry;
 
     public HybridTankOmni(DcMotor left, DcMotor right, Telemetry telemetry) {
+        this.telemetry = telemetry;
         this.left = left;
         this.right = right;
-        this.telemetry = telemetry;
+
+        set_mode_motors(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        set_mode_motors(DcMotor.RunMode.RUN_USING_ENCODER);
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        right.setDirection(DcMotorSimple.Direction.FORWARD);
+
         telemetry.addLine("Initialized Hybrid Tank/Omni Drivebase");
         telemetry.update();
     }
@@ -51,5 +58,10 @@ class HybridTankOmni extends DriveBase {
 
     public void report_encoder_ticks() {
 
+    }
+
+    private void set_mode_motors(DcMotor.RunMode mode) {
+        left.setMode(mode);
+        right.setMode(mode);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/DriveBaseTester.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode.debug;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.components.arm.Arm;
+
+import java.text.BreakIterator;
+
+@TeleOp(name = "Drive Base Tester", group = "Debug")
+public class DriveBaseTester extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addLine("Init");
+        telemetry.update();
+        final double ARM_JOYSTICK_MOVEMENT_THRESHOLD = 0.05;
+        gamepad1.setJoystickDeadzone((float)ARM_JOYSTICK_MOVEMENT_THRESHOLD);
+
+        DcMotor left = hardwareMap.dcMotor.get("left");
+        DcMotor right = hardwareMap.dcMotor.get("right");
+
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        right.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        telemetry.addLine("start");
+        telemetry.update();
+
+        boolean rb_pressed = false;
+        ControlMode mode = ControlMode.Tank;
+        while (opModeIsActive()) {
+            double leftx = gamepad1.left_stick_x;
+            double lefty = -gamepad1.left_stick_y;
+            double rightx = gamepad1.right_stick_x;
+            double righty = -gamepad1.right_stick_y;
+            boolean right_bumper = gamepad1.right_bumper;
+            telemetry.addData("Left x", leftx);
+            telemetry.addData("Left y", lefty);
+            telemetry.addData("Right x", rightx);
+            telemetry.addData("Right y", righty);
+            telemetry.addData("Right bumper", right_bumper);
+
+            if (!rb_pressed && right_bumper) {
+                mode = mode.toggle();
+            }
+            rb_pressed = right_bumper;
+            telemetry.addData("Mode", mode);
+
+            switch (mode) {
+                case Tank:
+                    left.setPower(lefty);
+                    right.setPower(righty);
+                case Joy:
+                    double turn_contrib = Math.abs(rightx);
+                    double throttle_contrib = 1 - turn_contrib;
+                    left.setPower(lefty * throttle_contrib + rightx);
+                    right.setPower(lefty * throttle_contrib - rightx);
+            }
+
+            telemetry.addData("R speed", right.getPower());
+            telemetry.addData("L speed", left.getPower());
+            telemetry.addData("R count", right.getCurrentPosition());
+            telemetry.addData("L count", left.getCurrentPosition());
+
+            telemetry.addLine("Running");
+            telemetry.update();
+        }
+        telemetry.addLine("finished");
+        telemetry.update();
+        stop();
+    }
+
+    enum ControlMode {
+        Tank, Joy;
+
+        private ControlMode next;
+        static {
+            Tank.next = Joy;
+            Joy.next = Tank;
+        }
+
+        public ControlMode toggle() {
+            return next;
+        }
+    }
+}
+
+
